ac48216302a8f3494d1f913eccec9684643bb8df
xyx107 <xyx107@outlook.com>
2021-01-02 19:45:55 +0800

    填几条数据

--- xblog.js
+++ xblog.js
@@ -72,51 +72,51 @@ const Article = mongoose.model('Article', new mongoose.Schema({
   }
 ))
 Article.insertMany([
-    // {
-    //     id: 1,
-    //     title: '解决前端跨域问题',
-    //     author: 'xyx107',
-    //     content: "一、通过jsonp跨域。因为html页面通过相应的标签可以加载其他域的静态资源，苏亦可以动态的创建script,请求带参网址，实现跨域。带参数的请求只适应于get请求，如var script = document.createElement('script');script.type = 'text/javascript';script.src = 'http://www.a.com/login?username=admin&callback=handlecallback'//传递一个回调函数给后端，方便后端执行这个回调函数document.head.appendChild(script);function handlecallback(res){alert(JSON.stringify(res));} 二、通过CORS跨域",
-    //     tags: 'JavaScript',
-    //     comments: {},
-    //     readNum: 5
-    // },
-    // {
-    //     id: 2,
-    //     title: 'JWT认证流程',
-    //     author: 'xyx107',
-    //     content: "1.JWT是什么 JWT(JSON Web Token)是基于token的鉴权机制类似于http协议也是无状态的，它不需要在服务端去保留用户的认证信息或者会话信息。这就意味着基于token认证机制的应用不需要去考虑用户在哪一台服务器登录了，这就为应用的扩展提供了便利。2.JWT认证流程 在前后端分离的项目中：前端将用户的登录信息发送给服务器；服务器接受请求后为用户生成独一无二的认证信息--token，传给客户端浏览器；客户端将token保存在cookie或者storage中；在之后访问客户端都携带这个token请求服务器；服务器验证token的值，如果验证成功则给客户端返回数据。服务器并不保存token。3.JWT的构成 JWS是JWT的一种实现，除了JWS外，JWE(JSON Web Encryption)也是JWT的一种实现。JWE的生成过程较为复杂，虽保证了安全性，但是降低了访问效率，JWS实现方式分为三部分：头部(header)，载荷(payload)，签证(signature)。",
-    //     tags: 'JavaScript',
-    //     comments: {},
-    //     readNum: 5 
-    // },
-    // {
-    //     id: 3,
-    //     title: '(阿里云）SSL证书从申请到安装',
-    //     author: 'xyx107',
-    //     content: "1.JWT是什么 JWT(JSON Web Token)是基于token的鉴权机制类似于http协议也是无状态的，它不需要在服务端去保留用户的认证信息或者会话信息。这就意味着基于token认证机制的应用不需要去考虑用户在哪一台服务器登录了，这就为应用的扩展提供了便利。2.JWT认证流程 在前后端分离的项目中：前端将用户的登录信息发送给服务器；服务器接受请求后为用户生成独一无二的认证信息--token，传给客户端浏览器；客户端将token保存在cookie或者storage中；在之后访问客户端都携带这个token请求服务器；服务器验证token的值，如果验证成功则给客户端返回数据。服务器并不保存token。3.JWT的构成 JWS是JWT的一种实现，除了JWS外，JWE(JSON Web Encryption)也是JWT的一种实现。JWE的生成过程较为复杂，虽保证了安全性，但是降低了访问效率，JWS实现方式分为三部分：头部(header)，载荷(payload)，签证(signature)。",
-    //     tags: 'JavaScript',
-    //     comments: {},
-    //     readNum: 5 
-    // },
-    // {
-    //     id: 4,
-    //     title: 'Mock.js测试接口使用方法',
-    //     author: 'xyx107',
-    //     content: "https://blog.csdn.net/xyx107/article/details/108002574",
-    //     tags: 'JavaScript',
-    //     comments: {},
-    //     readNum: 5 
-    // },
-    // {
-    //     id: 5,
-    //     title: 'leetcode 349 两个数组的交集 (JS数组方法)',
-    //     author: 'xyx107',
-    //     content: "https://blog.csdn.net/xyx107/article/details/108002574",
-    //     tags: 'JavaScript',
-    //     comments: {},
-    //     readNum: 5 
-    // },
+    {
+        id: 1,
+        title: '解决前端跨域问题',
+        author: 'xyx107',
+        content: "一、通过jsonp跨域。因为html页面通过相应的标签可以加载其他域的静态资源，苏亦可以动态的创建script,请求带参网址，实现跨域。带参数的请求只适应于get请求，如var script = document.createElement('script');script.type = 'text/javascript';script.src = 'http://www.a.com/login?username=admin&callback=handlecallback'//传递一个回调函数给后端，方便后端执行这个回调函数document.head.appendChild(script);function handlecallback(res){alert(JSON.stringify(res));} 二、通过CORS跨域",
+        tags: 'JavaScript',
+        comments: {},
+        readNum: 5
+    },
+    {
+        id: 2,
+        title: 'JWT认证流程',
+        author: 'xyx107',
+        content: "1.JWT是什么 JWT(JSON Web Token)是基于token的鉴权机制类似于http协议也是无状态的，它不需要在服务端去保留用户的认证信息或者会话信息。这就意味着基于token认证机制的应用不需要去考虑用户在哪一台服务器登录了，这就为应用的扩展提供了便利。2.JWT认证流程 在前后端分离的项目中：前端将用户的登录信息发送给服务器；服务器接受请求后为用户生成独一无二的认证信息--token，传给客户端浏览器；客户端将token保存在cookie或者storage中；在之后访问客户端都携带这个token请求服务器；服务器验证token的值，如果验证成功则给客户端返回数据。服务器并不保存token。3.JWT的构成 JWS是JWT的一种实现，除了JWS外，JWE(JSON Web Encryption)也是JWT的一种实现。JWE的生成过程较为复杂，虽保证了安全性，但是降低了访问效率，JWS实现方式分为三部分：头部(header)，载荷(payload)，签证(signature)。",
+        tags: 'JavaScript',
+        comments: {},
+        readNum: 5 
+    },
+    {
+        id: 3,
+        title: '(阿里云）SSL证书从申请到安装',
+        author: 'xyx107',
+        content: "1.JWT是什么 JWT(JSON Web Token)是基于token的鉴权机制类似于http协议也是无状态的，它不需要在服务端去保留用户的认证信息或者会话信息。这就意味着基于token认证机制的应用不需要去考虑用户在哪一台服务器登录了，这就为应用的扩展提供了便利。2.JWT认证流程 在前后端分离的项目中：前端将用户的登录信息发送给服务器；服务器接受请求后为用户生成独一无二的认证信息--token，传给客户端浏览器；客户端将token保存在cookie或者storage中；在之后访问客户端都携带这个token请求服务器；服务器验证token的值，如果验证成功则给客户端返回数据。服务器并不保存token。3.JWT的构成 JWS是JWT的一种实现，除了JWS外，JWE(JSON Web Encryption)也是JWT的一种实现。JWE的生成过程较为复杂，虽保证了安全性，但是降低了访问效率，JWS实现方式分为三部分：头部(header)，载荷(payload)，签证(signature)。",
+        tags: 'JavaScript',
+        comments: {},
+        readNum: 5 
+    },
+    {
+        id: 4,
+        title: 'Mock.js测试接口使用方法',
+        author: 'xyx107',
+        content: "https://blog.csdn.net/xyx107/article/details/108002574",
+        tags: 'JavaScript',
+        comments: {},
+        readNum: 5 
+    },
+    {
+        id: 5,
+        title: 'leetcode 349 两个数组的交集 (JS数组方法)',
+        author: 'xyx107',
+        content: "https://blog.csdn.net/xyx107/article/details/108002574",
+        tags: 'JavaScript',
+        comments: {},
+        readNum: 5 
+    }
 ])
 
 const User = mongoose.model('User', new mongoose.Schema({
@@ -130,13 +130,13 @@ const User = mongoose.model('User', new mongoose.Schema({
     },
     email: String
 }))
-// User.insertMany([
-//     { 
-//         userName: 'xingyu.yan',
-//         email: '1297184665@qq.com.com',
-//         password: '123456'
-//     }
-// ])
+User.insertMany([
+    { 
+        userName: 'archerx',
+        email: '98583067@qq.com',
+        password: 'archerx'
+    }
+])
 
 // 验证码
 app.post('/getCode', async (req, res) => {
